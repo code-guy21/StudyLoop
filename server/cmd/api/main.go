aa49a50f495419d8df024f6ea15c0f39875d4d78
Alexis San Javier <alexissj16@knights.ucf.edu>
2024-10-28 15:44:14 -0400

    configured docker container for postgres database and added yaml config file to load variables using viper

--- server/cmd/api/main.go
+++ server/cmd/api/main.go
@@ -3,30 +3,18 @@ package main
 import( 
 	"log"
 	"github.com/code-guy21/TutorLink/server/internal/app"
-	"github.com/code-guy21/TutorLink/server/internal/pkg"
-	"github.com/code-guy21/TutorLink/server/internal/app/models"
 )
 
 func main(){
-	config, _ := pkg.LoadConfig()
-	
 
-	db, err := pkg.InitDB(config)
+	application, err := app.NewApp()
 
 	if err != nil {
-		log.Fatalf("Failed to connect to the database: %v", err)
-	}
-	
-	log.Println("Database connection established", config.DatabaseName)
-
-	err = db.AutoMigrate(&models.Tutor{}, &models.Student{},&models.Payment{}, &models.Session{})
-
-	if err != nil {
-		log.Fatalf("Failed to run migrations: %v", err)
+		log.Fatalf("Failed to initalize app: %v", err)
 	}
 
-	if err := app.Run(config,db); err != nil {
-		log.Fatalf("Failed to run the application: %v", err)
+	if err := application.Run(); err != nil {
+		log.Fatalf("Error running app: %v", err)
 	}
 	
 }
--- server/internal/app/app.go
+++ server/internal/app/app.go
@@ -1,17 +1,78 @@
 package app 
 
 import (
-	"github.com/code-guy21/TutorLink/server/internal/app/routes"
-	"github.com/code-guy21/TutorLink/server/internal/pkg"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
+	"gorm.io/driver/postgres"
+	"github.com/spf13/viper"
+	"github.com/code-guy21/TutorLink/server/internal/handlers"
 )
 
-func Run(config *pkg.Config, db *gorm.DB) error {
+type App struct {
+	router *gin.Engine
+	db *gorm.DB
+}
+
+func (a *App) setupRoutes(){
+	a.router.GET("/health", handlers.HealthCheck)
+}
+
+func NewApp() (*App, error){
+	if err := initConfig(); err != nil{
+		return nil, fmt.Errorf("error initializing config: %w", err)
+	}
+
+	db, err := initDB()
+
+	if err != nil {
+		return nil, fmt.Errorf("error initializing database: %w", err)
+	}
 
 	router := gin.Default()
 
-	routes.InitRoutes(router,db)
+	app := &App {
+		router: router,
+		db: db,
+	}
 
-	return router.Run(":" + config.ServerPort)
+	app.setupRoutes()
+
+	return app, nil
+}
+
+func (a *App) Run() error {
+	return a.router.Run(fmt.Sprintf(":%d", viper.GetInt("server.port")))
+}
+
+
+
+func initConfig() error {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("./configs")
+
+	if err := viper.ReadInConfig(); err != nil {
+		return fmt.Errorf("error reading config file: %w", err)
+	}
+
+	return nil
+}
+
+func initDB() (*gorm.DB, error) {
+	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s",
+		viper.GetString("database.host"),
+		viper.GetInt("database.port"),
+		viper.GetString("database.user"),	
+		viper.GetString("database.password"),
+		viper.GetString("database.name"), viper.GetString("database.sslmode"),
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		return nil, fmt.Errorf("error connecting to database: %w", err)
+	}
+
+	return db, nil
 }
